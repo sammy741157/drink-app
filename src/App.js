4e66c78ef1353da11b064be5766cbe8340b4e686
Beverly Ndukwe <ndukwebeverly.com>
2025-07-24 18:16:50 +0300

    Initialize commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import UserDashboard from './Components/UserDashboard';
+import UserDashboard from './components/UserDashboard';
 import AdminDashboard from './components/AdminDashboard';
 import './App.css';
 
